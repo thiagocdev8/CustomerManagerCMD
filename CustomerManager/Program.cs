07a5844c53ce8f5b519c32b24415833225cda05c
Thiago <Jay@DESKTOP-TGBBAUH>
2025-07-07 13:41:21 -0300

    Adding initial menu

--- CustomerManager/Program.cs
+++ CustomerManager/Program.cs
@@ -2,9 +2,19 @@
 {
     class CustomerManager
     {
+
+
+        enum Menu { Listagem = 1, Adicionar = 2, Remover = 3, Sair = 4 }
         static void Main(string[] args)
         {
-            
+
+            Console.WriteLine("Customer/Client Manager 8.0 - Welcome!");
+            Console.WriteLine("Developed by SolarX!");
+            Console.WriteLine("");
+            Console.WriteLine("Please select an option:");
+            Console.WriteLine("1 - Customer List\n2-Add Customer\n3-Remove Customer\n4-Exit");
+
+
         }
     }
 }
